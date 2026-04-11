9ed60dd2dd59dbba4ba18c70a6071a4964867c28
Jack Perez <heartsdev2399@gmail.com>
2026-04-11 13:50:11 -0500

    Se agregó el getter y setter de usuarioupdatedto

--- backend/src/main/java/com/club_deportivo/backend/DTO/UsuarioDTO/UsuarioUpdateDTO.java
+++ backend/src/main/java/com/club_deportivo/backend/DTO/UsuarioDTO/UsuarioUpdateDTO.java
@@ -11,4 +11,36 @@ public class UsuarioUpdateDTO {
     private String num_celular;
     @NotBlank(message = "La dirección es obligatoria")
     private String direccion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNum_celular() {
+        return num_celular;
+    }
+
+    public void setNum_celular(String num_celular) {
+        this.num_celular = num_celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
